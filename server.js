1a9952b5afd897b01674af51c334034cde54603c
Assassin-dot <amieshsavla32@gmail.com>
2026-08-22 17:14:15 +0530

    update model

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function callGroq(messages) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      model: 'llama3-70b-8192',
+      model: 'llama-3.1-8b-instant',
       messages,
       temperature: 0.7,
       max_tokens: 2048
